bc9bfaa2633e298ed8ace1240ade5f510a58dc12
rgow <richardstewartgow@gmail.com>
2020-01-19 13:28:38 +0000

    rejig blade remplate relations

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -8,6 +8,6 @@ class IndexController
 {
     public function show()
     {
-        return view('index');
+        return view('pages.index');
     }
 }
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,7 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+        </ul>
+    </div>
+</div>
